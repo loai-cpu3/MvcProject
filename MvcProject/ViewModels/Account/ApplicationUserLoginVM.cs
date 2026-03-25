491203ffa506709229260a19cea24d932128f8a4
basantabdelmohsen <basantelsherif6@gmail.com>
2026-03-26 00:46:10 +0200

    Add MVC Project

---
+++ MvcProject/ViewModels/Account/ApplicationUserLoginVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcProject.ViewModels
+{
+    public class ApplicationUserLoginVM
+    {
+        public string Email { get; set; }
+        public bool RememberMe { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
